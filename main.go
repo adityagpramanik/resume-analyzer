ac40305bf951eeb7ba33510f7169f518ddd20521
adityagpramanik <adityagpramanik@gmail.com>
2024-07-09 01:15:00 +0530

    restructure: add routes and controllers

--- main.go
+++ main.go
@@ -6,21 +6,11 @@ import (
 	"log"
 	"net/http"
 
-	resumecontroller "resume-scanner.com/resume-scanner/controllers"
+	"resume-scanner.com/resume-scanner/routes"
 )
 
-func rootHandler(res http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodGet {
-		http.Error(res, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	fmt.Fprintf(res, "Hello world!\n")
-}
-
 func main() {
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/upload", resumecontroller.UploadHandler)
+	router := routes.SetupRoutes()
 	fmt.Println("Server listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
